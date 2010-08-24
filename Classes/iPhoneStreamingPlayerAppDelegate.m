49535a77f348d0d823964af354182b2b25f0dcce
Matt Gallagher <matt@oncebuilt.com>
2010-08-24 12:32:19 +1000

    Removed testing code that shouldn't have been in the last commit.

--- Classes/iPhoneStreamingPlayerAppDelegate.m
+++ Classes/iPhoneStreamingPlayerAppDelegate.m
@@ -21,10 +21,6 @@
 @synthesize viewController;
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
-		NSDictionary *credentialStorage =
-			[[NSURLCredentialStorage sharedCredentialStorage] allCredentials];
-		NSLog(@"Credentials: %@", credentialStorage);
-
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
