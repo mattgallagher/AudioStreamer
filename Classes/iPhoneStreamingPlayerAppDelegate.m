b526fa109860acb18f36124a4d316554b54f0d86
Matt Gallagher <matt@projectswithlove.com>
2011-01-08 11:58:56 +1100

    UI button fix.

--- Classes/iPhoneStreamingPlayerAppDelegate.m
+++ Classes/iPhoneStreamingPlayerAppDelegate.m
@@ -24,6 +24,8 @@
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
+	
+	[viewController buttonPressed:nil];
 }
 
 
